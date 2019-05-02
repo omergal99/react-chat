c39e025bbdc7c9dc2a14c1cb95e542a38bb22083
omergal99 <omergal99gm@gmail.com>
2019-05-02 22:14:24 +0300

    Updates

---
+++ precache-manifest.40142e3331a65dc2228e9aaa42ad5030.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcffa9c05dcef4ab949f431f657f81a5",
+    "url": "/react-chat/index.html"
+  },
+  {
+    "revision": "f4db9e6b4061f33757c7",
+    "url": "/react-chat/static/css/main.16ddf719.chunk.css"
+  },
+  {
+    "revision": "b2b9172d502c2f72a2d3",
+    "url": "/react-chat/static/js/2.59d0cd10.chunk.js"
+  },
+  {
+    "revision": "f4db9e6b4061f33757c7",
+    "url": "/react-chat/static/js/main.624f565f.chunk.js"
+  },
+  {
+    "revision": "49fde9a2889aba467256",
+    "url": "/react-chat/static/js/runtime~main.14e77f4a.js"
+  },
+  {
+    "revision": "56cb366910aa712773f8f2f7e7f42b9f",
+    "url": "/react-chat/static/media/Merienda-Regular.56cb3669.ttf"
+  },
+  {
+    "revision": "849e67957ea4dd5edb38eedb9d5743f1",
+    "url": "/react-chat/static/media/Quando-Regular.849e6795.ttf"
+  }
+]);
